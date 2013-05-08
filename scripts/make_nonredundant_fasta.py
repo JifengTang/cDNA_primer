9cbcb2270e43936355dad0f54a71a869f7a7fcc5
Magdoll <magdoll@gmail.com>
2013-05-08 14:06:04 -0700

    added min seqlen

--- scripts/make_nonredundant_fasta.py
+++ scripts/make_nonredundant_fasta.py
@@ -2,7 +2,7 @@ import os, sys
 import argparse
 from Bio import SeqIO
 
-def main(fasta_filename):
+def main(fasta_filename, min_seqlen):
     seen_id = set()
     seen_seq = {} # sequence --> index
     f = open(fasta_filename + '.nonredundant.fasta', 'w')
@@ -11,7 +11,9 @@ def main(fasta_filename):
     for r in SeqIO.parse(open(fasta_filename), 'fasta'):
         s = str(r.seq)
         id = r.id
-        if id in seen_id:
+        if len(s) < min_seqlen:
+            print >> sys.stderr, "{0} is too short. skipping this one".format(id)
+        elif id in seen_id:
             print >> sys.stderr, "id: {0} already seen. skipping this one".format(id)
         elif s in seen_seq:
             print >> sys.stderr, "seq of {0} already seen. skipping this one".format(id)
@@ -27,8 +29,9 @@ def main(fasta_filename):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Pre-processing for reference fasta files before importing to SMRTportal")
+    parser.add_argument("--min_seqlen", type=int, default=500, help="Minimum sequence length (def: 500)")
     parser.add_argument("fasta_filename")
-    
+
     args = parser.parse_args()
-    main(args.fasta_filename)
+    main(args.fasta_filename, args.min_seqlen)
 
